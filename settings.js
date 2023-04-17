1e240d863a2843bf3dc9a90ae9bf3709e16b8196
Medo90me8 <130885924+Medo90me8@users.noreply.github.com>
2023-04-17 15:11:22 +0200

    Update settings.js

--- settings.js
+++ settings.js
@@ -15,23 +15,23 @@ global.doc5 = 'application/pdf'
 global.doc6 = 'application/vnd.android.package-archive'
 
 //owmner v card
-global.owner = ['201098906252'] //ur owner number
-global.ownername = "ᗴᒪᘜᗩᘔᗩᖇ ᗩᒪᗯᗩᘔᗴᖇ❤️" //ur owner name
-global.ytname = "YT: ELGAZAR" //ur yt chanel name
-global.socialm = "GitHub: ELGAZAR" //ur github or insta name
+global.owner = ['201016892396'] //ur owner number
+global.ownername = "🔱..𝑴𝑬𝑫𝑶.🐉⁩" //ur owner name
+global.ytname = "YT: ///" //ur yt chanel name
+global.socialm = "GitHub: ///" //ur github or insta name
 global.location = "Africa, Egypt, Cairo" //ur location
 
 //bot bomdy 
-global.ownernomer = "201098906252" //ur number
-global.premium = ['201098906252'] //ur premium number
-global.botname = '❤️ᗴᒪᘜᗩᘔᗩᖇ ᗷOT❤️' //ur bot name
-global.linkz = "https://youtube.com/@ABDALLAH_MOHAMED" //your theme url which will be displayed on whatsapp
-global.websitex = "https://telegra.ph/file/ec763cc3e7c88d0e67682.jpg" //ur website to be displayed
-global.botscript = 'https://youtube.com/@ABDALLAH_MOHAMED' //script link
+global.ownernomer = "201016892396" //ur number
+global.premium = ['201016892396'] //ur premium number
+global.botname = 'Medo-BOT' //ur bot name
+global.linkz = "non" //your theme url which will be displayed on whatsapp
+global.websitex = "non" //ur website to be displayed
+global.botscript = 'non' //script link
 global.themeemoji = "♥" //ur theme emoji
-global.packname = "⍣الجزار الوزير يروحي⍣" //ur sticker watermark packname
-global.author = "⍣01098906252⍣" //ur sticker watermark author
-global.wm = "❤️ᗴᒪᘜᗩᘔᗩᖇ ᗷOT❤️." //ur watermark
+global.packname = "𝑴𝑬𝑫𝑶..⁩" //ur sticker watermark packname
+global.author = "⍣201016892396⍣" //ur sticker watermark author
+global.wm = "⁨🔱...𝑴𝑬𝑫𝑶..🐉⁩." //ur watermark
 
 // Other
 global.sessionName = 'session'
@@ -48,7 +48,7 @@ global.mess = {
     group: '『 الميزه دي للجروبات بس! 』',
     private: '『 الميزه دي في الخاص بس! 』',
     bot: '『 الميزه دي للبوت بس! 』',
-    wait: '『 استني شويه... 』',
+    wait: '『 اصبر🤌 』',
     linkm: '『 فين الرابط? 』',
     endLimit: 'انتهت صلاحية الحد اليومي الخاص بك ، وسيتم إعادة تعيين الحد كل 12 ساعة',
     nsfw: 'لم يتم تنشيط ميزة nsfw ، يرجى الاتصال بالمسؤول للتفعيل',
